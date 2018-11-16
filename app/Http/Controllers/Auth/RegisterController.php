5bb1fbc68915166c4b19b34f5ad8b66708c98fe8
jte1344 <jte1544@yahoo.com>
2018-11-16 12:42:43 -0700

    fixed registering for an account, (filled avatar, game 1-5, and bio with null values when a user registers)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
+
+
         ]);
     }
 
@@ -67,6 +69,13 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'avatar' => '',
+            'game1' => '',
+            'game2' => '',
+            'game3' => '',
+            'game4' => '',
+            'game5' => '',
+            'bio' => '',
         ]);
     }
 }
--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'avatar', 'game1', 'game2', 'game3', 'game4', 'game5', 'bio',
     ];
 
     /**
