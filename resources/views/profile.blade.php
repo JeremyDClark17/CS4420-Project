0d6d3c88a16a4b1fa9ea8256d95742cd47524d71
jte1344 <jte1544@yahoo.com>
2018-11-18 13:08:41 -0700

    small profile update

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -27,21 +27,18 @@
   </aside>
 </div>
 <hr>
-<div class="container">
+<div class="gallery">
   <a href=""> <!--INSERT game LINK here -->
-    <div class="thumbnailOne"><img src='/img/addGame.jpg' width="2000" class="cards-contain"/><!--INSERT game image here -->
+    <div class="thumbnail"><img src='/img/addGame.jpg' width="2000" class="cards-contain"/><!--INSERT game image here -->
       <h4>((Title of game))</h4>
       <p class="tag">Rating: ((Game's instance))</p>
       <li class="dropdown"> <a href="#" class="dropdown-toggle uname" style="font-size: 58px;" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true"><span class="caret"></span></a>
         <ul class="dropdown-menu ">
-          <li ><a class="text-left" href="">Edit Game 1</a></li>
+          <li ><a class="text-left" href="">Edit Game 2</a></li>
         </ul>
       </li>
     </div>
   </a>
-</div>
-<hr>
-<div class="gallery">
   <a href=""> <!--INSERT game LINK here -->
     <div class="thumbnail"><img src='/img/addGame.jpg' width="2000" class="cards-contain"/><!--INSERT game image here -->
       <h4>((Title of game))</h4>
